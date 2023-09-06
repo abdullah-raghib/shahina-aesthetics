0b039bac8bcf45fdf77ad6639c3c10bb6815b6b8
Abdullah Raghib <abdullah.raghib@vidyamantra.com>
2023-09-06 15:11:33 +0530

    image size for desktop fixed'

--- app/components/HomePage/MembershipBanner.jsx
+++ app/components/HomePage/MembershipBanner.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function MembershipBanner() {
   return (
-    <div className='w-full relative h-44 lg:h-96 rounded overflow-hidden'>
-      <div className='absolute right-2 pt-10 text-dark-green z-10 flex flex-col items-center'>
+    <div className='w-full relative h-44 lg:h-80 rounded overflow-hidden'>
+      <div className='absolute right-2 pt-10 lg:right-16 lg:top-16 text-dark-green z-10 flex flex-col items-center'>
         <h2 className='text-xl font-extrabold'>Membership</h2>
         <p className='text-sm'>Feel Great, Look Amazing</p>
         <button className='bg-dark-green mt-3 text-cream text-xs px-4 py-1 font-semibold'>
